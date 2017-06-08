cfd3bef9bd378d64d930c32a269e1ca336af4f08
unknown <>
2017-06-08 16:26:50 +0800

    选出A集合中元素的key属性跟B对象中value属性中的元素相同的元素,把他们的count，满3减1，输出减过之后的新A集

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let wordsList =[];
+  collectionA.forEach((item)=>{
+    wordsList.push(item.key);
+  });
+  objectB.value.forEach((item)=>{
+    if( collectionA[wordsList.indexOf(item)].count >= 3 )//满3减1，有几个3就减几个1
+      collectionA[wordsList.indexOf(item)].count-=parseInt((collectionA[wordsList.indexOf(item)].count/3));
+  })
+  return collectionA;
 }
