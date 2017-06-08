5764fdce217476168a60df2c788475b6352e9837
unknown <>
2017-06-08 10:08:16 +0800

    把A集合中相同的元素统计出数量section-2-practice-3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,43 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let ret = [];
+
+  collection.forEach(function (e) {
+    let refactor = AnalyseElement(e);
+    let pos = ret.findIndex(function (f) {
+      refactor = AnalyseElement(e);
+      return f.name === refactor[0];
+    });
+
+    if (pos === -1) {
+      ret.push({name: refactor[0], summary: refactor[1]});
+    } else {
+      ret[pos].summary += refactor[1];
+    }
+  });
+  return ret;
+}
+
+function AnalyseElement(element) {
+  let refactor = ['key', 0];
+  if (element.match('-')) {
+    refactor[0] = element.split('-')[0];
+    refactor[1] = element.split('-')[1];
+  }
+  else if (element.match(':')) {
+    refactor[0] = element.split(':')[0];
+    refactor[1] = element.split(':')[1];
+  }
+  else if (element.match(/\[/)) {
+    refactor[0] = element.split(/\[/)[0];
+    refactor[1] = element.split(/\[/)[1].split(/\]/)[0];
+  }
+  else {
+    refactor[0] = element;
+    refactor[1] = 1;
+  }
+  refactor[1] = Number(refactor[1]);
+
+  return refactor;
 }
