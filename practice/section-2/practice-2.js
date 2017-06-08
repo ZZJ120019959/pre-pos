17dac79aa18ee55ede286426e5b587e4a173b788
unknown <>
2017-06-08 10:05:21 +0800

    把A集合中相同的元素统计出数量setion-2-practice-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,24 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let ret = [];
+
+  collection.forEach(function (e) {
+    let pos = ret.findIndex(function (f) {
+      return f.key === e;
+    });
+    if (pos === -1) {
+      if (e.match('-')) {
+        let col = e.split('-');
+        ret.push({key: col[0], count: Number(col[1])});
+      }
+      else {
+        ret.push({key: e, count: 1});
+      }
+    } else {
+      ret[pos].count++;
+    }
+  });
+
+  return ret;
 }
